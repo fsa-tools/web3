2773c1d645acdbaacba5ce6df06de5cd87c732ea
Fábio Siqueira <fabioxxx@gmail.com>
2026-04-18 13:49:22 -0400

    feat: root index.ts — complete v1.7.1 reconstruction

---
+++ index.ts
@@ -0,0 +1,7 @@
+export * from "./src/abis/index.js";
+export * from "./src/constants/index.js";
+export * from "./src/math/index.js";
+export * from "./src/utils/index.js";
+export * as aerodrome from "./src/protocols/aerodrome/index.js";
+export * as uniswapV3 from "./src/protocols/uniswap-v3/index.js";
+export * as aave from "./src/protocols/aave/index.js";
